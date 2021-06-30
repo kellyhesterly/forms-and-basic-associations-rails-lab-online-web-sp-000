edc7e90a3e90fb3f0fd5059bafb5a4c0c163dfa5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-30 16:38:50 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
 
-  def song_artist_name
+  def artist_name
     self.artist ? self.artist.name :nil
   end
 end
