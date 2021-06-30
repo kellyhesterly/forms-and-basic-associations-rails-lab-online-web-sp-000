41f3b74b5f047602edf94107f7a2ca86344d9be5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-30 17:08:35 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,6 +22,9 @@ class Song < ActiveRecord::Base
 
   def note_contents=(contents)
     contents.each do |content|
+      if content.strip != ""
+        self.notes.build(content: content)
+      end
     end
   end
 
